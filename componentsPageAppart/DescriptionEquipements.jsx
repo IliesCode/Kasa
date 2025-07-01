3e35c8c38d1bf21351faf3dc226f9206d7144eef
IliesCode <iliesouahabi@live.fr>
2025-07-01 17:12:59 +0200

    finalisation de page appart hors responsive

--- componentsPageAppart/DescriptionEquipements.jsx
+++ componentsPageAppart/DescriptionEquipements.jsx
@@ -5,13 +5,19 @@ function DescriptionEquipements() {
   return (
     <div className='descriEqui'>
     <div className='style'>
-        <p>Description</p>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#ffffff" d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>
+        <div className='description'>
+            <p>Description</p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#ffffff" d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>        
         </div>
+        <p className='text'>Lorem Ipsu more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. aaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaa      aezaze azeaze ae aa aez aeaeaze aze azeazeazeazeae aze aze aze aze aze aze aze aze aaaa</p>
+    </div>
     <div className='style'>
-        <p>Equipements</p>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#ffffff" d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>
+        <div className='description'>
+            <p>Equipements</p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#ffffff" d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>        
         </div>
+        <p className='text'>Lorem Ipsu more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. aaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaa      aezaze azeaze ae aa aez aeaeaze aze azeazeazeazeae aze aze aze aze aze aze aze aze aaaa</p>
+    </div>
     </div>
   )
 }
